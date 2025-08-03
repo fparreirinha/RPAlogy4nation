e7c455cfb64bab9d0712e539a57671d84547f30c
Felipe Parreira <fparreirinha@msn.com>
2025-08-03 11:42:07 -0300

    fix: update welcome message to include user's name

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Welcome to RPAlogy4nation! What is the first building block of our lives as entrepreneurs.</h1>
+    return <h1>Thiago Welcome to RPAlogy4nation! What is the first building block of our lives as entrepreneurs.</h1>
 }
 
 export default Home;
